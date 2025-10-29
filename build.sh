80588f44163a2476a9c50dcaa5772c78a594854c
Bhuvanesh-Suresh <bhuvanesh1112006@gmail.com>
2025-10-29 22:07:34 +0530

    Add complete frontend + backend structure for Render deployment

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Build frontend
 cd frontend
 npm install
 npm run build
 
-# Move built files to backend/static
 mkdir -p ../backend/static
 cp -r dist/* ../backend/static/
 
-# Install backend requirements
 cd ../backend
 pip install -r requirements.txt
